85b1f0cc7f930669ef23c64b78729e693378166f
lorenzgruber <regru.lg@gmail.com>
2022-04-12 21:45:43 +0200

    create card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InventoryComponent } from './components/inventory/inventory.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, InventoryComponent],
+  declarations: [AppComponent, InventoryComponent, CardComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
