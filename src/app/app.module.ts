9e0757fe69d4a87fda3e776847a36577bd59e374
lorenzgruber <regru.lg@gmail.com>
2022-04-12 21:31:45 +0200

    create inventory component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, InventoryComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/inventory/inventory.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { InventorySlot } from '~models';
+import { CharacterService } from '~services';
+
+@Component({
+  selector: 'app-inventory',
+  templateUrl: './inventory.component.html',
+  styleUrls: ['./inventory.component.scss']
+})
+export class InventoryComponent implements OnInit {
+
+  inventory: InventorySlot[]
+
+  constructor(private characterService: CharacterService) { }
+
+  ngOnInit(): void {
+    this.characterService.getCharacter().subscribe(value => {
+      this.inventory = value.inventory;
+    })
+  }
+
+}
