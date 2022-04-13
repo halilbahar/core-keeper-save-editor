47ad1466d61eade43832015ca8e07d8a0581b866
lorenzgruber <regru.lg@gmail.com>
2022-04-13 17:13:09 +0200

    add component declerations to app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InventoryComponent } from './components/inventory/inventory.component';
 import { CardComponent } from './components/card/card.component';
+import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
-  declarations: [AppComponent, InventoryComponent, CardComponent],
+  declarations: [AppComponent, InventoryComponent, CardComponent, ItemComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
