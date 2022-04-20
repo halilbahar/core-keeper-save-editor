1f206c24210f737b7c420b8dd5da8c118db21546
halilbahar <halil.s.bahar@gmail.com>
2022-04-20 18:37:03 +0200

    Add default character to the character service

--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Character, InventorySlot } from '~models';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
+
+import { Character } from '~models';
+
+// eslint-disable-next-line no-restricted-imports
+import DefaultCharacter from '../../assets/default_character.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharacterService {
-  
-  $character: BehaviorSubject<Character> = new BehaviorSubject<Character>(undefined)
+  $character: BehaviorSubject<Character>;
 
-  constructor() {}
+  constructor() {
+    const defaultCharacter = DefaultCharacter as unknown as Character;
+    defaultCharacter.characterGuid = crypto.randomUUID().replace(/-/g, '');
+    this.$character = new BehaviorSubject<Character>(defaultCharacter);
+  }
 
   setCharacter(character: Character) {
-    this.$character.next(character)
+    this.$character.next(character);
   }
 }
