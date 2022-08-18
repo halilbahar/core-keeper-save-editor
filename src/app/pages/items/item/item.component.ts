04603b37a21fab2b5ff3676f9141ddddb50c24c9
lorenzgruber <regru.lg@gmail.com>
2022-08-18 20:24:55 +0200

    Display item-amount and durability-bars

--- src/app/pages/items/item/item.component.ts
+++ src/app/pages/items/item/item.component.ts
@@ -11,8 +11,12 @@ import { ItemDataService } from '~services';
 export class ItemComponent {
   private _objectID: number;
   private _scale: number = 1;
+  private _amount: number;
   @HostBinding('style') style: string = `--individual-scale: ${this._scale};`;
+
   itemData: ItemData;
+  durabilityProgress?: number;
+  durabilityBarColor?: string;
 
   @Input() set scale(value) {
     this._scale = value;
@@ -24,5 +28,31 @@ export class ItemComponent {
     this.itemData = this.itemDataService.getData(value);
   }
 
+  @Input() set amount(value) {
+    this._amount = value;
+    if (value && this.itemData.initialAmount > 1) {
+      this.durabilityProgress = (value / this.itemData.initialAmount) * 100;
+      this.durabilityBarColor = this.mapColor(this.durabilityProgress);
+    }
+  }
+
   constructor(private itemDataService: ItemDataService) {}
+
+  get amount() {
+    return this._amount;
+  }
+
+  private mapColor(progress: number): string {
+    const fullDurability = { r: 0, g: 240, b: 105 };
+    const lowDurability = { r: 245, g: 0, b: 65 };
+
+    const r =
+      lowDurability.r - Math.round(Math.abs(fullDurability.r - lowDurability.r) * 0.01 * progress);
+    const g =
+      lowDurability.g + Math.round(Math.abs(fullDurability.g - lowDurability.g) * 0.01 * progress);
+    const b =
+      lowDurability.b + Math.round(Math.abs(fullDurability.b - lowDurability.b) * 0.01 * progress);
+
+    return `rgb(${r},${g},${b})`;
+  }
 }
