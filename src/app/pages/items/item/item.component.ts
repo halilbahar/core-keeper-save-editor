692d101c770dcf78c8182f89ee9a897e2f180580
halilbahar <halil.s.bahar@gmail.com>
2022-08-18 15:42:29 +0200

    Be able to specify a scale for a single item

--- src/app/pages/items/item/item.component.ts
+++ src/app/pages/items/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 
 import { SelectedItemService } from 'src/app/services/selected-item.service';
 import { InventorySlot, ItemData } from '~models';
@@ -10,9 +10,16 @@ import { ItemDataService } from '~services';
   styleUrls: ['./item.component.scss']
 })
 export class ItemComponent implements OnInit {
+  private _scale: number = 1;
   @Input() isEditable: boolean;
   @Input() objectID: number;
+  @HostBinding('style') style: string = `--individual-scale: ${this._scale};`;
   itemData: ItemData;
+
+  @Input() set scale(value) {
+    this._scale = value;
+    this.style = `--individual-scale: ${this._scale};`;
+  }
 
   constructor(
     private itemDataService: ItemDataService,
