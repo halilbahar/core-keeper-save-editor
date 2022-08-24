0496e09ec85c0fff3bfc69484dd663eb0457ad7e
halilbahar <halil.s.bahar@gmail.com>
2022-08-25 01:47:30 +0200

    Create enum for bags

---
+++ src/app/enums/bag.ts
@@ -0,0 +1,8 @@
+export enum Bag {
+  CavePouch = 8400,
+  GhormsStomachBag = 8401,
+  ScarletShellBackpack = 8402,
+  ExplorerBackpack = 8403,
+  OctarineBag = 8404,
+  MorphasBubbleBag = 8405
+}
--- src/app/enums/index.ts
+++ src/app/enums/index.ts
@@ -1,3 +1,4 @@
+export * from './bag';
 export * from './item-rarity';
 export * from './item-categories';
 export * from './equipment-placeholder';
