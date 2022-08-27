f97906925ed77270b1bcfd60fd31e2a72e6d97c8
lorenzgruber <regru.lg@gmail.com>
2022-08-27 03:20:19 +0200

    Change skill levels with + and - buttons

--- src/app/pages/skill-page/components/skill-list/skill-list.component.ts
+++ src/app/pages/skill-page/components/skill-list/skill-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
 import { SkillTalentService } from 'src/app/services/skill-talent.service';
-import { Skill } from '~models';
+import { Character, Skill } from '~models';
 import { CharacterService } from '~services';
 
 @UntilDestroy()
@@ -13,6 +13,7 @@ import { CharacterService } from '~services';
 })
 export class SkillListComponent implements OnInit {
   skills: Skill[];
+  character: Character;
   selectedSkillID: number;
 
   constructor(
@@ -23,6 +24,7 @@ export class SkillListComponent implements OnInit {
   ngOnInit(): void {
     this.characterService.$character.pipe(untilDestroyed(this)).subscribe(value => {
       this.skills = value.skills;
+      this.character = value;
     });
     this.skillTalentService.$selectedSkill.pipe(untilDestroyed(this)).subscribe(value => {
       this.selectedSkillID = value;
@@ -33,9 +35,38 @@ export class SkillListComponent implements OnInit {
     this.skillTalentService.setSelectedSkill(skillID);
   }
 
-  onLevelIncreaseClick(): void {}
+  onLevelIncreaseClick(): void {
+    this.setSelectedSkillLevel(
+      this.skillTalentService.getLevelByXp(
+        this.selectedSkillID,
+        this.skills[this.selectedSkillID].value
+      ) + 1
+    );
+  }
 
-  onLevelDecreaseClick(): void {}
+  onLevelDecreaseClick(): void {
+    this.setSelectedSkillLevel(
+      this.skillTalentService.getLevelByXp(
+        this.selectedSkillID,
+        this.skills[this.selectedSkillID].value
+      ) - 1
+    );
+  }
+
+  setSelectedSkillLevel(level: number): void {
+    this.characterService.setCharacter({
+      ...this.character,
+      skills: this.character.skills.map(skill => {
+        return {
+          ...skill,
+          value:
+            skill.skillID === this.selectedSkillID
+              ? this.skillTalentService.getXpForLevel(this.selectedSkillID, level)
+              : skill.value
+        };
+      })
+    });
+  }
 
   getSelectedSkillName(): string {
     return this.skillTalentService.getSkillName(this.selectedSkillID);
