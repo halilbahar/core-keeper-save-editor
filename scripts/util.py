816601ff3a83f7b2b996ab4952c7e35e2c7fd4fa
halilbahar <halil.s.bahar@gmail.com>
2022-12-09 14:15:10 +0100

    Add methods for food and variation for later

--- scripts/util.py
+++ scripts/util.py
@@ -66,3 +66,31 @@ def get_enum(path) -> dict:
             result[int(enum_value)] = enum_name
 
     return result
+
+
+def first_ingredient_is_primary(ingredient1, ingredient2):
+    var1 = ingredient1 * 2 + 0x157b9 + ingredient2
+    var1 = ((var1 ^ 0x3d0000) >> 0x10 ^ var1) * 9
+    var2 = (var1 >> 4 ^ var1) * 0x27d4eb2d
+    var2 = var2 >> 0xf ^ var2
+    var2 = var2 ^ var2 << 0xd
+    var2 = var2 ^ var2 >> 0x11
+    var1 = ingredient2 * 2 + 0x157b9 + ingredient1
+    var1 = ((var1 ^ 0x3d0000) >> 0x10 ^ var1) * 9
+    var1 = (var1 >> 4 ^ var1) * 0x27d4eb2d
+    var1 = var1 >> 0xf ^ var1
+    var1 = var1 ^ var1 << 0xd
+    var1 = var1 ^ var1 >> 0x11
+    return ((var1 << 5 ^ var1) >> 9 | 0x3f800000) - 1.0 < ((var2 << 5 ^ var2) >> 9 | 0x3f800000) - 1.0
+
+
+def get_food(first_object_id, second_object_id):
+    first_ingredient_primary = first_ingredient_is_primary(first_object_id, second_object_id)
+    if first_ingredient_primary:
+        result_ingredient1 = first_object_id
+        result_ingredient2 = second_object_id
+    else:
+        result_ingredient1 = second_object_id
+        result_ingredient2 = first_object_id
+
+    return result_ingredient1 * 10000 + result_ingredient2
