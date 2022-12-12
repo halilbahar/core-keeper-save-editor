28185422146c54f01982cc272d5758c400f43ff5
halilbahar <halil.s.bahar@gmail.com>
2022-12-12 20:21:22 +0100

    Create service for getting the talents

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -2,5 +2,6 @@ export * from './aes.service';
 export * from './character.service';
 export * from './item-data.service';
 export * from './drag-n-drop.service';
+export * from './talent-data.service';
 export * from './selected-item.service';
 export * from './extracted-data.service';
