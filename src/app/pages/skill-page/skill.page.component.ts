2323df15e0ada532ba6d04b2a4b152521c371cd1
lorenzgruber <regru.lg@gmail.com>
2022-12-13 20:58:52 +0100

    Remove debug talent-tree from skill.page

--- src/app/pages/skill-page/skill.page.component.ts
+++ src/app/pages/skill-page/skill.page.component.ts
@@ -1,31 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { Component } from '@angular/core';
 
-import { SkillTalentTree } from '~models';
-import { CharacterService } from '~services';
-
-@UntilDestroy()
 @Component({
   selector: 'app-skill-page',
   templateUrl: './skill.page.component.html',
   styleUrls: ['./skill.page.component.scss']
 })
-export class SkillPageComponent implements OnInit {
-  skillTalentTreeDatas: SkillTalentTree[];
-  debugTree: SkillTalentTree;
-  constructor(private characterService: CharacterService) {}
-
-  ngOnInit(): void {
-    this.characterService.$character.pipe(untilDestroyed(this)).subscribe(character => {
-      this.skillTalentTreeDatas = character.skillTalentTreeDatas;
-      this.debugTree = this.skillTalentTreeDatas.find(tree => tree.skillTreeID === 0);
-      const pointsLength = this.debugTree.points.length;
-      if (pointsLength < 8) {
-        const diff = 8 - pointsLength;
-        for (let i = 0; i < diff; i++) {
-          this.debugTree.points.push(0);
-        }
-      }
-    });
-  }
+export class SkillPageComponent {
+  constructor() {}
 }
