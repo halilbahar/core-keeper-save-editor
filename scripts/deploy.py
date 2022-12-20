8942b20b4415d87d2eb22597497b0110b9ec8aa4
halilbahar <halil.s.bahar@gmail.com>
2022-12-20 20:17:51 +0100

    Deploy to data instead of config

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
+import os
 from os import path
 from re import sub
-from shutil import copytree, copy2
+from shutil import copytree, copy2, rmtree
 
 from PIL import Image
 
 
 # https://stackoverflow.com/a/12514470/11125147
-def copy_to(src, dst_folder, dst=None):
+def copy_to(src, src_folder, dst_folder, dst=None):
     if dst is None:
         dst = src
 
-    s = path.join('out', src)
+    s = path.join('out', os.path.join(src_folder, src))
     d = path.join(dst_folder, dst)
     if path.isdir(s):
         copytree(s, d, False, None)
@@ -19,31 +20,38 @@ def copy_to(src, dst_folder, dst=None):
         copy2(s, d)
 
 
-def copy_to_assets(src):
-    copy_to(src, '../src/assets')
+def copy_to_assets(src, src_folder, dst=None):
+    copy_to(src, src_folder, '../src/assets', dst=dst)
 
 
-def copy_to_config(src):
-    copy_to(src, '../src/app/config')
+def copy_to_data(src, src_folder, dst=None):
+    copy_to(src, src_folder, '../src/app/data', dst=dst)
 
 
 if __name__ == '__main__':
-    # Copy files
-    copy_to_config('data.json')
-    # copy_to_config('talent-data.json')
-    copy_to_assets('item-spritesheet.png')
-    item_spritesheet = Image.open('out/item-spritesheet.png')
+    # Copy items
+    if os.path.isdir('out/item'):
+        copy_to_data('item-data.json', 'item')
+        copy_to_assets('item-spritesheet.png', 'item')
+        # Replace variables
+        with open('../src/variables.scss', 'r') as file:
+            file_data = file.read()
+        # Replace the target string
+        item_spritesheet = Image.open('out/item/item-spritesheet.png')
+        file_data = sub(
+            r'--spritesheet-width: \d+px;', '--spritesheet-width: %dpx;' % item_spritesheet.size[0],
+            file_data
+        )
+        # Write the file out again
+        with open('../src/variables.scss', 'w') as file:
+            file.write(file_data)
 
-    # Replace variables
-    with open('../src/variables.scss', 'r') as file:
-        file_data = file.read()
-    # Replace the target string
-    file_data = sub(r'--spritesheet-width: \d+px;', '--spritesheet-width: %dpx;' % item_spritesheet.size[0], file_data)
-    # Write the file out again
-    with open('../src/variables.scss', 'w') as file:
-        file.write(file_data)
+    if os.path.isdir('out/talents'):
+        copy_to_data('talent-data.json', 'talents')
+        # Delete old talents
+        rmtree('../src/assets/talents', ignore_errors=True)
+        # Copy new talents
+        copy_to_assets('image', 'talents', dst='talents')
 
-    # Delete old talents
-    # rmtree('../src/assets/talents', ignore_errors=True)
-    # Copy new talents
-    # copy_to_assets('talents')
+    if os.path.isdir('out/conditions'):
+        copy_to_data('condition-data.json', 'conditions')
---
+++ src/app/data/index.ts
@@ -0,0 +1,3 @@
+export { default as ItemData } from './item-data.json';
+export { default as TalentData } from './talent-data.json';
+export { default as ConditionData } from './condition-data.json';
