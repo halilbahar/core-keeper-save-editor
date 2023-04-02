7acd36fc440c00e13148221d6ca11ad7ba68cfcb
tealeon <leon.tea@hotmail.com>
2023-04-02 18:34:03 +0200

    update comment

--- src/app/services/item-set.service.ts
+++ src/app/services/item-set.service.ts
@@ -48,8 +48,8 @@ export class ItemSetService {
       pieces.push({ name, isHighlighted: false });
     }
 
-    // remove redundant items
-    // https://stackoverflow.com/questions/15125920/how-to-get-distinct-values-from-an-array-of-objects-in-javascript/58429784#58429784
+    // remove duplicates like rings
+    // https://stackoverflow.com/a/58429784/11125147
     result.pieces = [...new Map(pieces.map(item => [item['name'], item])).values()];
     const wearingPieces = result.pieces.filter(piece => piece.isHighlighted).length;
 
